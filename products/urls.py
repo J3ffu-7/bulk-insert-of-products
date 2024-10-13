5b2246da3a672922012b23181f9855777839c16f
“J3ffu-7” <mwajeujeff@gmail.com>
2024-10-14 00:23:38 +0300

    Deployment link and updated routing

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.product_list, name='home'),  # Root URL redirects to product list
     path('products/bulk-insert/', views.bulk_insert_products, name='bulk_insert_products'),
     path('products/', views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
